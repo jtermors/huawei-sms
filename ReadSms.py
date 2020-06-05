fc7922763a071c5fa21997cd6cb76a907f19ea52
jtermors <johan@ter-mors.eu>
2020-06-05 11:19:49 +0200

    Create ReadSms.py

---
+++ ReadSms.py
@@ -0,0 +1,16 @@
+from huawei_lte_api.Client import Client
+from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
+from huawei_lte_api.Connection import Connection
+from huawei_lte_api.enums.sms import BoxTypeEnum
+from huawei_lte_api.enums.sms import TextModeEnum
+
+connection = AuthorizedConnection('http://admin:password@192.168.8.1/')
+client = Client(connection)
+
+#Read SMS
+sms = client.sms.get_sms_list(1, BoxTypeEnum.LOCAL_INBOX, 1, 0, 0, 1)
+print(sms)
+
+#Send SMS
+#print(client.sms.config())
+#print(client.sms.send_sms([1144], "SALDO"))
